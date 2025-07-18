829e99bdbfa2643b83c89c68ed088c3f171ce89a
SokChanmakara <sokchanmakara04@gmail.com>
2025-07-18 21:49:11 +0700

    update it to use localStorage instead

--- task-manager/script.js
+++ task-manager/script.js
@@ -5,40 +5,29 @@ let tasks = [];
 const taskForm = document.getElementById("task-form");
 const taskList = document.getElementById("task-list");
 const taskInput = document.getElementById("task-input");
+const STORAGE_KEY = 'taskManager .tasks';
 
 // ========== COOKIE HELPER FUNCTIONS ===================
-function setCookie(name, value, maxAgeSeconds) {
-  document.cookie = `${name}=${encodeURIComponent(
-    value
-  )}; max-age=${maxAgeSeconds}; path=/;`;
+function setLocalStorage(name, value) {
+  localStorage.setItem(name, JSON.stringify(value))
 }
 
-function getCookie(name) {
-  const value = `; ${document.cookie}`;
-  const parts = value.split(`; ${name}=`);
-  if (parts.length === 2)
-    return decodeURIComponent(parts.pop().split(";").shift());
-  return null;
+function getLocalStorage(name) {
+  const data = localStorage.getItem(name);
+  return data? JSON.parse(data) : null;
 }
 
 // ========== FUNCTION TO SAVE TASKS TO COOKIE ===========
 
-function saveTaskToCookie() {
-  const json = JSON.stringify(tasks);
-  setCookie("tasks", json, 3600);
+function saveTaskToLocalStorage() {
+  setLocalStorage('tasks', tasks);
 }
 
 // ========== FUNCTION TO LOAD TASKS FROM COOKIE =========
 
-function loadTaskFromCookie() {
-  const tasksCookie = getCookie("tasks");
-  if (tasksCookie) {
-    try {
-      tasks = JSON.parse(tasksCookie);
-    } catch (error) {
-      tasks = [];
-    }
-  }
+function loadTaskFromLocalStorage() {
+  const saved = getLocalStorage('tasks');
+  tasks = saved || [];
 }
 
 // ========== FUNCTION TO RENDER ========================
@@ -74,7 +63,7 @@ function renderTask() {
         if (e.key === "Enter") input.blur();
       });
 
-      input.addEventListener("blur", ()=>{
+      input.addEventListener("blur", () => {
         updateTask(index, input.value.trim());
       });
 
@@ -98,29 +87,29 @@ function renderTask() {
 
 //============== FUNCTION TO ADD TASK ===================
 function addTask(title) {
-  tasks.push({ title, completed:false });
-  saveTaskToCookie();
+  tasks.push({ title, completed: false });
+  saveTaskToLocalStorage();
   renderTask();
 }
 
 //============== FUNCTION TO DELETE TASK =================
 function deleteTask(index) {
   tasks.splice(index, 1);
-  saveTaskToCookie();
+  saveTaskToLocalStorage();
   renderTask();
 }
 
 //================ TOGGLE COMPLETE ========================
 function toggleComplete(index) {
   tasks[index].completed = !tasks[index].completed;
-  saveTaskToCookie();
+  saveTaskToLocalStorage();
   renderTask();
 }
 //================ UPDATE TASK ============================
 function updateTask(index, newTitle) {
   if (newTitle) {
     tasks[index].title = newTitle;
-    saveTaskToCookie();
+    saveTaskToLocalStorage();
     renderTask();
   }
 }
@@ -134,5 +123,5 @@ taskForm.addEventListener("submit", (e) => {
     taskInput.focus();
   }
 });
-loadTaskFromCookie();
+loadTaskFromLocalStorage();
 renderTask();
